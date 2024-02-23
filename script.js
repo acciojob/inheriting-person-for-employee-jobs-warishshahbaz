6afb27927ffa514e316bb2b5a6183ecf61329521
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-23 18:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // complete this js code
-function Person(name, age) {
-	 constructor(name, age) {
+class Person {
+  constructor(name, age) {
     this.name = name;
     this.age = age;
   }
@@ -10,8 +10,8 @@ function Person(name, age) {
   }
 }
 
-function Employee(name, age, jobTitle) {
-	constructor(name, age, jobTitle) {
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
     super(name, age);
     this.jobTitle = jobTitle;
   }
@@ -20,7 +20,6 @@ function Employee(name, age, jobTitle) {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
   }
 }
-
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
