0a0e82a24333ad22ae89ec51384c17507b826050
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-23 18:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	 constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-function Employee(name, age, jobTitle) {}
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
+}
+
+function Employee(name, age, jobTitle) {
+	constructor(name, age, jobTitle) {
+    super(name, age);
+    this.jobTitle = jobTitle;
+  }
+
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
+}
 
 // Do not change code below this line
 window.Person = Person;
