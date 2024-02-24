ee095ee1e86e767e61abf977dd42fabb1d153c1b
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-24 12:40:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 // complete this js code
-function Person(name, age) {
-	 constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
 
-  greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
+To fix the code, we need to correct the syntax errors and ensure that the inheritance is properly set up. Here's the corrected version:
+
+javascript
+Copy code
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 }
+
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
 
 function Employee(name, age, jobTitle) {
-	constructor(name, age, jobTitle) {
-    super(name, age);
-    this.jobTitle = jobTitle;
-  }
-
-  jobGreet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-  }
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
-Employee.prototype = new Person;
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
